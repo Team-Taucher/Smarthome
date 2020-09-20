c1467ef591bea97019f7c80899b97577c745459c
Stefan Ninkovic <stefannninkovic@gmail.com>
2020-09-21 00:03:40 +0200

    CHANGE: Added repository and data-class for a device

---
+++ src/Common.Data/Device.cs
@@ -0,0 +1,23 @@
+﻿namespace Gelp.SmartHome.Common.Data
+{
+    /// <summary>
+    /// Contains all informations about a user
+    /// </summary>
+    public class Device : BaseObject
+    {
+        /// <summary>
+        /// Name of the device
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Port of the device
+        /// </summary>
+        public int Port { get; set; }
+
+        /// <summary>
+        /// Indicates whether the device is activated or not
+        /// </summary>
+        public bool IsActivated { get; set; }
+    }
+}
