a8927d37ff4b153aeb0877f062722dad78175bde
David Chin <david.chin@bluewin.ch>
2020-09-24 23:01:33 +0200

    ligma balls danilo

---
+++ src/Communication.Web/Models/DashboardModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Gelp.SmartHome.Common.Data;
+
+namespace Gelp.SmartHome.Communication.Web.Models
+{
+    public class DashboardModel
+    {
+        public User User { get; set; }
+        
+        public List<Device> Devices { get; set; }
+    }
+}
