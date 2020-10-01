735f8bf54c0575eba05984e4c3b0f743c9dae1db
David Chin <david.chin@bluewin.ch>
2020-10-01 20:42:10 +0200

    stores the logged in users username and password as a base64 decoded string in a cookie which the user can write in the login view.

--- src/Communication.Web/Controllers/DashboardController.cs
+++ src/Communication.Web/Controllers/DashboardController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Gelp.SmartHome.Common.Data;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Gelp.SmartHome.Communication.Web.Controllers {
