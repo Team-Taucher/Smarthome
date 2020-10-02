fda1b0e8f1c758a90c04a9c0b4d7c8c2f1097da2
TilloRazzai <tillo.razzai@gmail.com>
2020-10-02 07:32:36 +0200

    ongoing development: LoginScreen

---
+++ src/Communication.Web/Controllers/LoginController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Gelp.SmartHome.Common.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gelp.SmartHome.Communication.Web.Controllers {
+    public class LoginController : Controller {
+        // GET: LoginController
+        public ActionResult Index() {
+            return View("Login");
+        }
+
+
+        // POST: LoginController/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(User user) {
+            try
+            {
+                //if login was successful
+                HttpContext.Response.Cookies.Append("CookieKey", Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes($"{user.Username}, {user.Password}")));
+                return RedirectToAction("Dashboard", "Dashboard");
+            } catch {
+                return View();
+            }
+        }
+    }
+}
